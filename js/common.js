57c4417e65ab1457bcc63dff0775bd2fd76d538e
bbibbu0223 <sweetbox95@naver.com>
2021-07-29 16:09:28 +0900

    0729

--- js/common.js
+++ js/common.js
@@ -11,6 +11,21 @@ $(function(){
             menuIc.classList.toggle('active');
             menu.classList.toggle('active');
         });
+
+        const header = document.querySelector('header');
+        const headerHeight = header.getBoundingClientRect().height;
+
+        window.addEventListener("scroll",function(){
+            if (window.scrollY > headerHeight) {
+                header.setAttribute("style", "background: #292f4e;");
+                header.style.zIndex = "1";  
+            } else {
+            header.setAttribute("style", "background: transparent;");
+            header.style.zIndex = "1";  
+            }
+        });
+
+        
     }
 
     
