961fecefc9710338b3b346f4dba072f4e261b5d0
bbibbu0223 <sweetbox95@naver.com>
2021-07-29 17:17:29 +0900

    0729(2)

--- js/common.js
+++ js/common.js
@@ -25,6 +25,15 @@ $(function(){
             }
         });
 
+        const circle = document.querySelector(".circle");
+
+        document.addEventListener("mousemove", (e) => {
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+            circle.style.left = mouseX + 'px';
+            circle.style.top = mouseY + 'px';
+            circle.style.zIndex ="-1";
+        });
         
     }
 
